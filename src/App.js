660519bff929d876a6311e31fdc14a905572e874
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-05-07 15:41:35 +0530

    Basic UI done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
-import Navigation from './components/Navigation/Navigation.js'
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+
+const particlesOptions = {
+  //customize this to your liking
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}/>
       <Navigation/>
-  {/*<Logo/>
+      <Logo/>
+      <Rank/>
       <ImageLinkForm/>
-      <FaceRecognition/>
+      {
+      /*<FaceRecognition/>
   */}
     </div>
   );

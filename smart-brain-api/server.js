6315e19fcf03769bfd290a5242e8f8660a9a2127
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-05-10 11:49:33 +0530

    implemented signin,register requests

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,10 +1,78 @@
 const express = require('express');
+const bodyParser=require('body-parser');
+
 const app=express();
+app.use(bodyParser.json());
+const database={
+    users:[
+        {
+            id:'123',
+            name:'Jhon',
+            email:'jhon@gmail.com',
+            password:'cookies',
+            entries:0,
+            joined:new Date()
+        },
+        {
+            id:'124',
+            name:'Sally',
+            email:'sally@gmail.com',
+            password:'bananas',
+            entries:0,
+            joined:new Date()
+        }
+    ]
+}
 
 app.get('/',(req,res)=>{
-    res.send('this is working');
+    res.json(database.users);
 })
+
+app.post('/signin',(req,res)=>{
+    if(req.body.email===database.users[0].email &&
+        req.body.password===database.users[0].password ){
+            res.json('success');
+    }else{
+        res.json('error ');
+    }
+})
+
+app.post('/register',(req,res)=>{
+    const {email,name,password}=req.body;
+    database.users.push({
+        id:'125',
+        name:name,
+        email:email,
+        password:password,
+        entries:0,
+        joined: new Date(),
+    })
+    res.json(database.users[database.users.length-1]);
+    
+})
+
+app.get('/profile/:id',(req,res)=>{
+    const {id}=req.params;
+    let found=false;
+    database.users.forEach((user)=>{
+        if(user.id===id){
+            found=true;
+            return res.json(user);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found')
+    }
+})
+
 
 app.listen(3000,()=>{
     console.log("hi");
 })
+
+/*
+    res this is working
+    /profile/:userId --> GET = user
+    /image --> PUT --> user 
+
+*/ 
